6aea214b283f9ffd9ee2b4b7ed448f546042bfb6
NickPowers43 <nickpowers43@gmail.com>
2016-07-21 15:23:55 -0700

    Added text loading

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 //main.js
 
+function loadText(path, loadSuccess) {
+	$.ajax({
+		url : path,
+		dataType: "text",
+		success : loadSuccess
+	});
+}
 
+//load and compile shader
+function createShader(gl, type, sourcePath, createSuccess) {
+	
+	function loadSuccess(source) {
+		
+		var shader = gl.createShader(type);
+		gl.shaderSource(shader, source);
+		gl.compileShader(shader);
+		
+		createSuccess(shader);
+	}
+	
+	loadText(sourcePath, loadSuccess);
+}
