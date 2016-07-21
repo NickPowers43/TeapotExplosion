05801f1fedf5c1bdf80fb69357ca55a899a5d37f
NickPowers43 <nickpowers43@gmail.com>
2016-07-21 16:03:28 -0700

    Added Program compiler/linker

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 //main.js
 
+//Get GL context
+var canvas = document.getElementById("canvas")
+var gl = canvas.getContext("webgl");
+
+var SHADER_TYPE_NAME_MAP = {};
+SHADER_TYPE_NAME_MAP[gl.VERTEX_SHADER] = "vertex";
+SHADER_TYPE_NAME_MAP[gl.FRAGMENT_SHADER] = "fragment";
+
 function loadText(path, loadSuccess) {
 	$.ajax({
 		url : path,
@@ -17,8 +25,42 @@ function createShader(gl, type, sourcePath, createSuccess) {
 		gl.shaderSource(shader, source);
 		gl.compileShader(shader);
 		
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			alert("Failed to compile " + SHADER_TYPE_NAME_MAP[type] + " shader:\n" + gl.getShaderInfoLog(shader));
+			return;
+		}
+		
 		createSuccess(shader);
 	}
 	
 	loadText(sourcePath, loadSuccess);
 }
+
+	
+	//load shaders
+		
+		var program = gl.createProgram();
+		gl.attachShader(program, vs);
+		gl.attachShader(program, fs);
+		gl.linkProgram(program);
+		
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			return;
+		}
+		
+		success(program);
+	});});
+}
+
+
+loadText("models/utah-teapot.json", function(data) {
+	
+	var model = JSON.parse(data);
+	
+	var program = createProgram(gl, "shaders/test.vs", "shaders/test.fs", function (program){
+		alert("program created successfully");
+	});
+	
+});
+
+
