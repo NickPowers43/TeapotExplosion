558b566342fa181a243c303dc254b2a288e5a296
NickPowers43 <nickpowers43@gmail.com>
2016-07-21 17:11:25 -0700

    Added basic shaders

--- shaders/test.fs
+++ shaders/test.fs
@@ -1,1 +1,5 @@
-test
+precision mediump float;
+
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
--- shaders/test.vs
+++ shaders/test.vs
@@ -1,1 +1,6 @@
-test
+
+attribute vec3 position;
+
+void main() {
+    gl_Position = vec4(position,1.0);
+}  
